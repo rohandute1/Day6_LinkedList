55fbb63b94d236110140272443ec4b1fcc78f1aa
Rohan <rohandute1@gmail.com>
2023-10-02 12:41:44 +0530

    [rohan] Created simple linked list

---
+++ Day6_LinkedListProblem/LinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_LinkedListProblem
+{
+    class LinkedList
+    {
+        private Node head;
+
+        public LinkedList()
+        {
+            this.head = null;
+        }
+
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node current = head;
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+                current.next = newNode;
+            }
+        }
+
+        public void Display()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.Write(current.data + " ");
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Day6_LinkedListProblem/Program.cs
+++ Day6_LinkedListProblem/Program.cs
@@ -12,6 +12,14 @@ namespace Day6_LinkedListProblem
         {
             Console.WriteLine("Welcome to LinkedList Problems.");
 
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+
+            Console.WriteLine("Linked List: ");
+            linkedList.Display();
+
             Console.ReadLine();
         }
     }
